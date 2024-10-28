2cee511c1270d09b4c2a62299d548de351dd1d58
Florian <hoelken@uni-wuppertal.de>
2024-10-28 13:33:31 +0100

    improvements for request dataset

--- frontend/src/components/DatasetDetailModal.js
+++ frontend/src/components/DatasetDetailModal.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DatasetRequestModal from './DatasetRequestModal';
 
 const DatasetDetailModal = ({ dataset, onClose }) => {
-  const [showRequestModal, setShowRequestModal] = useState(false); // State to control request modal visibility
+  const [showRequestModal, setShowRequestModal] = useState(false);
 
   if (!dataset) return null;
 
@@ -15,62 +15,46 @@ const DatasetDetailModal = ({ dataset, onClose }) => {
     <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1" role="dialog">
       <div className="modal-dialog modal-custom-width" role="document">
         <div className="modal-content">
-          <div className="modal-header">
+          {/* Adjust modal-header with justify-content-between */}
+          <div className="modal-header d-flex align-items-center justify-content-between">
             <h5 className="modal-title">Dataset Details</h5>
-            <button type="button" className="close" onClick={onClose} aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+
+            <div className="d-flex align-items-center">
+              {!dataset.is_public && (
+                <button
+                  className="btn btn-primary mr-2"
+                  onClick={() => setShowRequestModal(true)}
+                >
+                  Request Dataset
+                </button>
+              )}
+              <button type="button" className="close" onClick={onClose} aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
           </div>
           <div className="modal-body">
             <ul className="list-group">
-              <li className="list-group-item">
-                <strong>ID:</strong> {dataset.id}
-              </li>
-              <li className="list-group-item">
-                <strong>Name:</strong> {dataset.name}
-              </li>
-              <li className="list-group-item">
-                <strong>Description:</strong> {dataset.description}
-              </li>
-              <li className="list-group-item">
-                <strong>Creation Date:</strong> {formatDate(dataset.creation_date)}
-              </li>
-              <li className="list-group-item">
-                <strong>Last Modified Date:</strong> {formatDate(dataset.last_modified_date)}
-              </li>
-              <li className="list-group-item">
-                <strong>Owner:</strong> {dataset.owner.name}
-              </li>
-              <li className="list-group-item">
-                <strong>Contact:</strong> {dataset.contact.name}
-              </li>
+              <li className="list-group-item"><strong>ID:</strong> {dataset.id}</li>
+              <li className="list-group-item"><strong>Name:</strong> {dataset.name}</li>
+              <li className="list-group-item"><strong>Description:</strong> {dataset.description}</li>
+              <li className="list-group-item"><strong>Creation Date:</strong> {formatDate(dataset.creation_date)}</li>
+              <li className="list-group-item"><strong>Last Modified Date:</strong> {formatDate(dataset.last_modified_date)}</li>
+              <li className="list-group-item"><strong>Owner:</strong> {dataset.owner.name}</li>
+              <li className="list-group-item"><strong>Contact:</strong> {dataset.contact.name}</li>
               <li className="list-group-item">
                 <strong>Is Public:</strong> {dataset.is_public ? (
                   <i className="fa-solid fa-check"></i>
                 ) : (
                   <i className="fa-solid fa-xmark"></i>
-                  
                 )}
               </li>
-              <li className="list-group-item">
-                <strong>File Path:</strong> {dataset.file_path}
-              </li>
+              <li className="list-group-item"><strong>File Path:</strong> {dataset.file_path}</li>
             </ul>
-
-            {/* Conditional rendering of Request Dataset button */}
-            {!dataset.is_public && (
-              <button
-                className="btn btn-primary mt-3"
-                onClick={() => setShowRequestModal(true)}
-              >
-                Request Dataset
-              </button>
-            )}
           </div>
         </div>
       </div>
 
-      {/* Render DatasetRequestModal when showRequestModal is true */}
       {showRequestModal && (
         <DatasetRequestModal
           dataset={dataset}
