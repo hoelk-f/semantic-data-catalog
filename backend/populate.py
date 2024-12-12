3dceb33fe27f7bacd926c077bc84379946943701
Florian Hölken <hoelken@uni-wuppertal.de>
2024-12-12 15:28:16 +0100

    updated populate

--- backend/populate.py
+++ backend/populate.py
@@ -4,20 +4,15 @@ from crud import create_person, create_dataset, create_dataspace, get_dataspace_
 from schemas import PersonCreate, DatasetCreate, DataspaceCreate, PodCreate
 from datetime import datetime
 
-# Path to the file to be added as a blob
 BLOB_FILE_PATH = "public/assets/files/test.ttl"
 
-# Testdaten für Personen, Datensätze und Dataspaces
 def populate_db(db: Session):
-    # Read the blob file content
     with open(BLOB_FILE_PATH, "rb") as f:
         file_blob = f.read()
 
-    # Testpersonen
     person1 = create_person(db, PersonCreate(name="Alice Example", email="alice@example.com", phone_number="123456789"))
     person2 = create_person(db, PersonCreate(name="Bob Example", email="bob@example.com", phone_number="987654321"))
 
-    # Testdatensätze
     dataset1 = DatasetCreate(
         name="Temperature Data 1",
         description="Test Temperature Dataset 1",
@@ -61,23 +56,20 @@ def populate_db(db: Session):
     create_dataset(db, dataset2, file_blob=file_blob)
     create_dataset(db, dataset3, file_blob=file_blob)
 
-    # Test-Dataspaces
     dataspace1 = DataspaceCreate(name="solid-dataspace-1", link="http://localhost:3000")
     dataspace2 = DataspaceCreate(name="solid-dataspace-2", link="http://localhost:3001")
     dataspace3 = DataspaceCreate(name="solid-dataspace-3", link="http://localhost:3002")
 
-    # Only create dataspace if it doesn't already exist
     for dataspace in [dataspace1, dataspace2, dataspace3]:
         existing_dataspace = get_dataspace_by_name(db, dataspace.name)
         if not existing_dataspace:
             create_dataspace(db, dataspace)
 
-    # Add initial pods
     pod1 = PodCreate(name="data-pod-1", server_id=1, path="testpod1s1/")
     pod2 = PodCreate(name="data-pod-2", server_id=2, path="testpod1s2/")
     pod3 = PodCreate(name="data-pod-3", server_id=3, path="testpod1s3/")
 
-    for pod in [pod1, pod2]:
+    for pod in [pod1, pod2, pod3]:
         existing_pod = get_pod_by_name(db, pod.name)
         if not existing_pod:
             create_pod(db, pod)
