2b78392c26e32730820bc08949c4e891644842d2
tidujiie <tidujiie@tidujiie.com>
2025-09-04 12:38:59 +0200

    added tmdt logo

--- frontend/src/components/FooterBar.js
+++ frontend/src/components/FooterBar.js
@@ -9,6 +9,11 @@ const FooterBar = () => {
           alt="Logo BUW"
           className="logo-small"
         />
+        <img
+          src={process.env.PUBLIC_URL + '/assets/images/Logo_TMDT.png'}
+          alt="Logo TMDT"
+          className="logo-medium"
+        />
       </div>
 
       <div className="footer-version">
